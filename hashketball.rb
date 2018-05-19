952e3e67442437eaac4110e2d718fc8ebfe597bd
Brad Newman <newmanbradm@gmail.com>
2018-05-19 22:25:02 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -57,7 +57,23 @@ def game_hash
       }
     }
     away: {
-      
+      team_name: "Charlotte Hornets",
+      colors: ["Turquoise", "Purple"],
+      players: {
+        "Jeff Adrien" => {
+          number: 4, 
+          shoe: 18,
+          points: 10,
+          rebounds: 1,
+          assists: 1,
+          steals: 2,
+          blocks: 7,
+          slam_dunks: 2
+        },
+        "Bismak Biyombo" => {
+          
+        }
+      }
     }
   }
 
