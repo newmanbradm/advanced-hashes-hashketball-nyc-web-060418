9d4a5a61e7ffb83a8495ebfb4e5b8995a1d18253
Brad Newman <newmanbradm@gmail.com>
2018-05-19 22:25:32 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -71,6 +71,16 @@ def game_hash
           slam_dunks: 2
         },
         "Bismak Biyombo" => {
+          number: 0,
+          shoe: 16,
+          points: 12,
+          rebounds: 4,
+          assists: 7,
+          steals: 7,
+          blocks: 15,
+          slam_dunks: 10
+        },
+        "DeSanga Diop" => {
           
         }
       }
