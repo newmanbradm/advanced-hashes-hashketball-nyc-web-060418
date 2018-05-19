e158b12aa1d418b93292276a4f7b9009185bfbb9
Brad Newman <newmanbradm@gmail.com>
2018-05-19 22:14:28 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -45,9 +45,19 @@ def game_hash
           slam_dunks: 5
         },
         "Jason Terry" => {
-          
+          number: 31,
+          shoe: 15,
+          points: 19,
+          rebounds: 2,
+          assists: 2,
+          steals: 4,
+          blocks: 11,
+          slam_dunks: 1
         }
       }
+    }
+    away: {
+      
     }
   }
 
